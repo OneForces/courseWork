888fe070f57ddd1e22fda191d71323347570d0a0
OneForces <teller2003@mail.ru>
2021-03-25 20:05:26 +0300

    test3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//test2gfdgfdg
+//test2gfdgfdgflkdsklgdklfgkl
 public class Main extends Application {
 
     @Override
